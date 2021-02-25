5e2721eb812b3a8d90905bdc276d3b5112bd4d5b
itoshi1104 <ssj3itoshi1104@gmail.com>
2021-02-25 10:30:02 +0000

    key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
+  Refile.secret_key = 'abf37b79024c13ba80364d19dfa2e153b75e4da91d129cc7bf0e0e35fa481eba495d3dd9aad7d4d6aed79c2db88c10d049fee41437f09a6a43c00bd4d62fd4aa'
 
   def set_search
     # @search = Article.search(params[:q])
