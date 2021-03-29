50db4afbb043a8265e6987833aed1f1e42f18025
itoshi1104 <ssj3itoshi1104@gmail.com>
2021-03-29 08:42:47 +0000

    タスク登録日付逆行阻止

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,13 @@ class Task < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true,  length: { maximum: 50 }
-  validates :body, length: { maximum: 100 }
+  validates :body, presence: true,  length: { maximum: 100 }
+  validate :start_finish_check
+  
+  def start_finish_check
+    if start_date > end_date
+      errors.add(:finish_time, "が開始日時より前の日付では登録できません。")
+    end
+  end
+  
 end
